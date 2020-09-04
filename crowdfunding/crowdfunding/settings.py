9efdf1ab09aaac28cda940aa0b95f6076180b8a4
SamaraLove <samaramglove@gmail.com>
2020-09-04 20:30:07 +0800

    disabled sessionauth

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -54,7 +54,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework.authentication.TokenAuthentication',
-    'rest_framework.authentication.SessionAuthentication',
+    # 'rest_framework.authentication.SessionAuthentication',
     # 'rest_framework.authentication.BasicAuthentication',
 
 
